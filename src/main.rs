fd7468ef962f3dd053469cf66a7171ae18f12db6
Ryan Warsaw <email@ryan-w.me>
2020-05-10 15:53:04 -0500

    Lock scroll to bottom of list on message send

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,15 @@ fn main() {
     let mut cursive_root = Cursive::default();
 
     let mut message_pane = LinearLayout::vertical();
-    message_pane.add_child(TextView::empty()
-        .with_name("message_history")
-        .full_screen()
+
+    let message_history = TextView::empty()
         .scrollable()
         .show_scrollbars(false)
-        .scroll_strategy(ScrollStrategy::StickToBottom));
+        .scroll_strategy(ScrollStrategy::StickToBottom)
+        .full_screen()
+        .with_name("message_history");
+    message_pane.add_child(message_history);
+
     message_pane.add_child(EditView::new()
         .on_submit(handle_message_submit)
         .full_width()
@@ -27,8 +30,9 @@ fn main() {
 // TODO: There is some bug here where if you send messages super quick it'll hold the previous message state and just mutate it <x> characters.
 // TODO: so if you send something like 00000, super quickly then send a 1, it'll do: 100000 for some reason.
 fn handle_message_submit(cursive_root: &mut Cursive, message: &str) {
-    cursive_root.call_on_name("message_history", |view: &mut TextView | {
-        view.append(message.to_owned() + "\n");
+    cursive_root.call_on_name("message_history", |view: &mut ResizedView<ScrollView<TextView>> | {
+        view.get_inner_mut().get_inner_mut().append(message.to_owned() + "\n");
+        view.get_inner_mut().set_scroll_strategy(ScrollStrategy::StickToBottom);
     });
 
     // Reset the send message input once we have rendered the message.

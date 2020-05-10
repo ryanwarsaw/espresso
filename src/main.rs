cbfdb9209d9752f58645213df63d69a177be9fc0
Ryan Warsaw <email@ryan-w.me>
2020-05-10 18:54:41 -0500

    Fix rendering issue with invisible layers

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-use cursive::{Cursive, View};
-use cursive::theme::{Color, PaletteColor, Theme};
+use cursive::{Cursive};
+use cursive::theme::{Color, PaletteColor, Theme, Effect, BaseColor};
 use cursive::traits::{Nameable, Resizable, Scrollable};
 use cursive::views::{Dialog, EditView, LinearLayout, ListView, ResizedView, TextView, ViewRef, ScrollView};
 use cursive::view::ScrollStrategy;
 
 fn main() {
     let mut cursive_root = Cursive::default();
-
     let mut message_pane = LinearLayout::vertical();
-
     let message_history = TextView::empty()
         .scrollable()
         .show_scrollbars(false)
@@ -23,12 +21,10 @@ fn main() {
         .with_name("send_message"));
 
     cursive_root.set_theme(configure_theme(&cursive_root));
-    cursive_root.screen_mut().add_transparent_layer(message_pane);
+    cursive_root.screen_mut().add_fullscreen_layer(message_pane);
     cursive_root.run();
 }
 
-// TODO: There is some bug here where if you send messages super quick it'll hold the previous message state and just mutate it <x> characters.
-// TODO: so if you send something like 00000, super quickly then send a 1, it'll do: 100000 for some reason.
 fn handle_message_submit(cursive_root: &mut Cursive, message: &str) {
     cursive_root.call_on_name("message_history", |view: &mut ResizedView<ScrollView<TextView>> | {
         view.get_inner_mut().get_inner_mut().append(message.to_owned() + "\n");
@@ -43,7 +39,7 @@ fn handle_message_submit(cursive_root: &mut Cursive, message: &str) {
 
 fn configure_theme(cursive_root: &Cursive) -> Theme {
     let mut theme = cursive_root.current_theme().clone();
-    theme.palette[PaletteColor::Background] = Color::TerminalDefault;
-
+    theme.palette[PaletteColor::View] = Color::TerminalDefault;
+    theme.palette[PaletteColor::Primary] = Color::TerminalDefault;
     return theme;
 }

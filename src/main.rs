393150aed07ec153b7898877b2835b2d03c213c6
Ryan Warsaw <email@ryan-w.me>
2020-05-10 19:25:37 -0500

    Cleanup the layout a little bit

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,38 @@
-use cursive::{Cursive};
-use cursive::theme::{Color, PaletteColor, Theme, Effect, BaseColor};
+use cursive::Cursive;
+use cursive::theme::{Color, PaletteColor, Theme};
 use cursive::traits::{Nameable, Resizable, Scrollable};
-use cursive::views::{Dialog, EditView, LinearLayout, ListView, ResizedView, TextView, ViewRef, ScrollView};
 use cursive::view::ScrollStrategy;
+use cursive::views::{EditView, LinearLayout, ResizedView, ScrollView, TextView};
 
 fn main() {
     let mut cursive_root = Cursive::default();
-    let mut message_pane = LinearLayout::vertical();
-    let message_history = TextView::empty()
-        .scrollable()
-        .show_scrollbars(false)
-        .scroll_strategy(ScrollStrategy::StickToBottom)
-        .full_screen()
-        .with_name("message_history");
-    message_pane.add_child(message_history);
-
-    message_pane.add_child(EditView::new()
-        .on_submit(handle_message_submit)
-        .full_width()
-        .with_name("send_message"));
-
     cursive_root.set_theme(configure_theme(&cursive_root));
-    cursive_root.screen_mut().add_fullscreen_layer(message_pane);
+    cursive_root.screen_mut().add_fullscreen_layer(create_message_view());
     cursive_root.run();
 }
 
+fn create_message_view() -> LinearLayout {
+    LinearLayout::vertical()
+        .child(TextView::empty()
+            .scrollable()
+            .show_scrollbars(false)
+            .scroll_strategy(ScrollStrategy::StickToBottom)
+            .full_screen()
+            .with_name("message_history"))
+        .child(EditView::new()
+            .on_submit(handle_message_submit)
+            .full_width()
+            .with_name("send_message"))
+}
+
 fn handle_message_submit(cursive_root: &mut Cursive, message: &str) {
-    cursive_root.call_on_name("message_history", |view: &mut ResizedView<ScrollView<TextView>> | {
+    cursive_root.call_on_name("message_history", |view: &mut ResizedView<ScrollView<TextView>>| {
         view.get_inner_mut().get_inner_mut().append(message.to_owned() + "\n");
         view.get_inner_mut().set_scroll_strategy(ScrollStrategy::StickToBottom);
     });
 
     // Reset the send message input once we have rendered the message.
-    cursive_root.call_on_name("send_message", | view: &mut ResizedView<EditView> | {
+    cursive_root.call_on_name("send_message", |view: &mut ResizedView<EditView>| {
         view.get_inner_mut().set_content("");
     });
 }
